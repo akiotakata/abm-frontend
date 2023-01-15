2ddd393d492ab21d1ed0f81e57ff82526c973996
Akio Takata <akiotakata1@gmail.com>
2023-01-15 16:26:10 -0300

    se agrega funcionalidad form creación

--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -1,11 +1,11 @@
 import Form from 'react-bootstrap/Form';
 
-const Input = ({ id, label }) => {
+const Input = ({ name, label, onChange }) => {
   return (
     <>
-      <Form.Group className="mb-3" controlId={id}>
+      <Form.Group className="mb-3" controlId={name}>
         <Form.Label>{label}</Form.Label>
-        <Form.Control type="text" />
+        <Form.Control type="text" name={name} onChange={onChange} />
       </Form.Group>
     </>
   );
--- src/components/vehicle/Create.js
+++ src/components/vehicle/Create.js
@@ -1,19 +1,91 @@
+import { useState } from 'react';
+import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Input from '../form/Input';
 
 const Create = () => {
+  const initialValues = {
+    tipo_vehiculo: '',
+    cantidad_ruedas: '',
+    marca: '',
+    modelo: '',
+    patente: '',
+    numero_chasis: '',
+    kilometros: ''
+  };
+
+  const [vehicle, setVehicle] = useState(initialValues);
+
+  const onInputChange = (event) => {
+    const { name, value } = event.target;
+
+    setVehicle({ ...vehicle, [name]: value });
+  };
+
+  const createVehicle = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        `http://localhost/api/vehicles`,
+        vehicle
+      );
+
+      setVehicle(initialValues);
+
+      // agregar redirect a home
+    } catch (error) {
+      console.log('Error:', error.message);
+    }
+  };
+
   return (
     <div>
       <h1>Crear Vehículo</h1>
-      <Form>
-        <Input id="vehicleType" label="Tipo de vehículo" />
-        <Input id="wheels" label="Cantidad de ruedas" />
-        <Input id="brand" label="Marca" />
-        <Input id="model" label="Modelo" />
-        <Input id="patent" label="Patente" />
-        <Input id="chassisNumber" label="Número de Chasis" />
-        <Input id="kilometers" label="KMs recorridos" />
+      <Form onSubmit={createVehicle}>
+        <Input
+          name="tipo_vehiculo"
+          label="Tipo de vehículo"
+          value={vehicle.tipo_vehiculo}
+          onChange={onInputChange}
+        />
+        <Input
+          name="cantidad_ruedas"
+          label="Cantidad de ruedas"
+          value={vehicle.cantidad_ruedas}
+          onChange={onInputChange}
+        />
+        <Input
+          name="marca"
+          label="Marca"
+          value={vehicle.marca}
+          onChange={onInputChange}
+        />
+        <Input
+          name="modelo"
+          label="Modelo"
+          value={vehicle.modelo}
+          onChange={onInputChange}
+        />
+        <Input
+          name="patente"
+          label="Patente"
+          value={vehicle.patente}
+          onChange={onInputChange}
+        />
+        <Input
+          name="numero_chasis"
+          label="Número de Chasis"
+          value={vehicle.numero_chasis}
+          onChange={onInputChange}
+        />
+        <Input
+          name="kilometros"
+          label="KMs recorridos"
+          value={vehicle.kilometros}
+          onChange={onInputChange}
+        />
 
         <Button variant="primary" type="submit">
           Crear
